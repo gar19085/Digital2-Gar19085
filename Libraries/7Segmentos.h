94e3ee15a32b74a3462e2f489042a8cfd7c5aecf
gar19085 <68084823+gar19085@users.noreply.github.com>
2021-02-01 23:54:52 -0600

    Lab02 V1.0

---
+++ Libraries/7Segmentos.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   
+ * Author: RODRIGO GARCIA
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef _7SEGMENTOS_H
+#define	_7SEGMENTOS_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
