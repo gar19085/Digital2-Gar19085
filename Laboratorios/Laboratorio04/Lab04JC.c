c996e9a007243d25ff310a1e377b315f5770a53a
gar19085 <68084823+gar19085@users.noreply.github.com>
2021-03-21 21:32:08 -0600

    Lab04 V1.2 Final

--- Laboratorios/Laboratorio04/Lab04JC.c
+++ Laboratorios/Laboratorio04/Lab04JC.c
@@ -10,7 +10,7 @@ uint8_t PL1, PL2;
 uint8_t FLG;
 uint8_t FL1, FL2;
 
-const int buttonPin = 19;     // the number of the pushbutton pin
+const int Push3 = 19;     // the number of the pushbutton pin
 int breadboardButtonState = 0; // variable for reading the BB button status
 int launchpadButtonState1 = 0; // variable for reading the LP button state
 int launchpadButtonState2 = 0;
@@ -30,9 +30,21 @@ void setup() {
   pinMode(PA_3, OUTPUT);
   pinMode(PA_4, OUTPUT);
   pinMode(PB_6, OUTPUT);
+  pinMode(PB_7, OUTPUT);
   pinMode(PB_3, OUTPUT);
   pinMode(PE_0, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+  
+  pinMode(PA_7, OUTPUT);
+  pinMode(PA_6, OUTPUT);
+  pinMode(PE_3, OUTPUT);
+  pinMode(PE_2, OUTPUT);
+  pinMode(PE_1, OUTPUT);
+  pinMode(PD_3, OUTPUT);
+  pinMode(PD_2, OUTPUT);
+  pinMode(PB_1, OUTPUT);
+  pinMode(PB_0, OUTPUT);  
+  
+  pinMode(Push3, INPUT_PULLUP);
   pinMode(PUSH1, INPUT_PULLUP);
   pinMode(PUSH2, INPUT_PULLUP);
 }
@@ -52,12 +64,21 @@ void loop() {
          LEDJ1();
       }
     }
+    if (launchpadButtonState2 == 0) {
+     FL2 = 1;
+    }else {
+      if(FL2 == 1 && launchpadButtonState2 == 1){
+         FL2=0;
+         PL2++;
+         LEDJ2();
+      }
+    }
   }
 }
 
 void START(void) {
   // read the state of the push button value:
-  breadboardButtonState = digitalRead(buttonPin);
+  breadboardButtonState = digitalRead(Push3);
   if (breadboardButtonState == 0) {
     PL1 = 0;
     PL2 = 0;
@@ -67,8 +88,19 @@ void START(void) {
     digitalWrite(PA_3, LOW);
     digitalWrite(PA_4, LOW);
     digitalWrite(PB_6, LOW);
+    digitalWrite(PB_7, LOW);
     digitalWrite(PB_3, LOW);
     digitalWrite(PE_0, LOW);    
+    
+    digitalWrite(PA_7, LOW);
+    digitalWrite(PA_6, LOW);
+    digitalWrite(PE_3, LOW);
+    digitalWrite(PE_2, LOW);
+    digitalWrite(PE_1, LOW);
+    digitalWrite(PD_3, LOW);
+    digitalWrite(PD_2, LOW);
+    digitalWrite(PB_1, LOW);
+    digitalWrite(PB_0, LOW);  
     semaforo();
   }
 }
@@ -92,34 +124,100 @@ void LEDJ1(void) {
             break;
         case 1:
             digitalWrite(PD_6, HIGH);
+            delay(400);
             break;            
         case 2:
             digitalWrite(PD_6, LOW);
             digitalWrite(PD_7, HIGH);
+            delay(400);
             break;
         case 3:
             digitalWrite(PD_7, LOW);
             digitalWrite(PA_2, HIGH);
+            delay(400);
             break;
         case 4:
             digitalWrite(PA_2, LOW);
             digitalWrite(PA_3, HIGH);
+            delay(400);
             break;
         case 5:
             digitalWrite(PA_3, LOW);
             digitalWrite(PA_4, HIGH);
+            delay(400);
             break;
         case 6:
             digitalWrite(PA_4, LOW);
             digitalWrite(PB_6, HIGH);
+            delay(400);
             break;
         case 7:
             digitalWrite(PB_6, LOW);
-            digitalWrite(PB_3, HIGH);
+            digitalWrite(PB_7, HIGH);
+            delay(400);
             break;
         case 8:
+            digitalWrite(PB_7, LOW);
+            digitalWrite(PB_3, HIGH);
+            delay(400);
+            break;
+        case 9:
             digitalWrite(PB_3, LOW);
             digitalWrite(PE_0, HIGH);
+            delay(400);
+            FLG = 0;
+            break;            
+  }
+}
+void LEDJ2(void) {
+  switch(PL2){
+        case 0:
+            digitalWrite(PA_7, HIGH); 
             break;
+        case 1:
+            digitalWrite(PA_7, HIGH);
+            delay(400);
+            break;            
+        case 2:
+            digitalWrite(PA_7, LOW);
+            digitalWrite(PA_6, HIGH);
+            delay(400);
+            break;
+        case 3:
+            digitalWrite(PA_6, LOW);
+            digitalWrite(PE_3, HIGH);
+            delay(400);
+            break;
+        case 4:
+            digitalWrite(PE_3, LOW);
+            digitalWrite(PE_2, HIGH);
+            delay(400);
+            break;
+        case 5:
+            digitalWrite(PE_2, LOW);
+            digitalWrite(PE_1, HIGH);
+            delay(400);
+            break;
+        case 6:
+            digitalWrite(PE_1, LOW);
+            digitalWrite(PD_3, HIGH);
+            delay(400);
+            break;
+        case 7:
+            digitalWrite(PD_3, LOW);
+            digitalWrite(PD_2, HIGH);
+            delay(400);
+            break;
+        case 8:
+            digitalWrite(PD_2, LOW);
+            digitalWrite(PB_1, HIGH);
+            delay(400);
+            break;
+        case 9:
+            digitalWrite(PB_1, LOW);
+            digitalWrite(PB_0, HIGH);
+            delay(400);
+            FLG = 0;
+            break;            
   }
 }
