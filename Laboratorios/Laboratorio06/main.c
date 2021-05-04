b300341872419397e91cf91eac7f9b0e24b2b3e7
gar19085 <68084823+gar19085@users.noreply.github.com>
2021-05-03 21:26:40 -0600

    Lab06 V1.2

--- Laboratorios/Laboratorio06/main.c
+++ Laboratorios/Laboratorio06/main.c
@@ -7,24 +7,42 @@
 #include "driverlib/pin_map.h"
 #include "driverlib/debug.h"
 
+uint8_t FLG = 0;
+
 int main(void)
 {
-
     SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_4);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 
     while(1)
     {
-        // Turn on the LED
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x08);
-        // Delay for a bit
-        SysCtlDelay(2000000);
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x12);
-        SysCtlDelay(2000000);
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x24);
-        SysCtlDelay(2000000);
+        if(GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4) == 0){
+            FLG = 1;
+        }
+        if(GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4) == 16 && FLG == 1){
+            FLG = 0;
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_3);
+            SysCtlDelay(20000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x00);
+            SysCtlDelay(3000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_3);
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x00);
+            SysCtlDelay(3000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_3);
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x00);
+            SysCtlDelay(3000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 10);
+            SysCtlDelay(30000000);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_1);
+            SysCtlDelay(20000000);
+        }
     }
 }
+
