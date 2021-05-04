cd302d04fafb21771ba8b66fd7a1b9103c29c985
gar19085 <68084823+gar19085@users.noreply.github.com>
2021-05-03 19:19:54 -0600

    Lab06 V1.1

--- Laboratorios/Laboratorio06/main.c
+++ Laboratorios/Laboratorio06/main.c
@@ -4,10 +4,11 @@
 #include "inc/hw_memmap.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/debug.h"
 
 int main(void)
 {
-    uint8_t LED = 2;
 
     SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 
@@ -18,14 +19,12 @@ int main(void)
     while(1)
     {
         // Turn on the LED
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, LED);
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x08);
         // Delay for a bit
         SysCtlDelay(2000000);
-        // Cycle through Red, Green and Blue LEDs
-        if (LED == 8) {
-            LED = 2;
-        } else {
-                LED = LED*2;
-        }
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x12);
+        SysCtlDelay(2000000);
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x24);
+        SysCtlDelay(2000000);
     }
 }
