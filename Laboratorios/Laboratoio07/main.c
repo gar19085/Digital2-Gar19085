9633d6a0d84d4482420b22f5b91c9bbb96dfbcbd
gar19085 <68084823+gar19085@users.noreply.github.com>
2021-05-09 23:55:22 -0600

    Lab07 V1.0

---
+++ Laboratorios/Laboratoio07/main.c
@@ -0,0 +1,81 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "driverlib/uart.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+
+/**
+ * main.c
+ */
+
+/*
+ * Variables
+ * */
+uint8_t GRN;
+uint8_t RED;
+uint8_t BLU;
+
+/*
+ * Interrupciones
+ * */
+void Timer0IntHandler(void){
+    TimerIntClear(TIMER0_BASE, TIMER_TMA_TIMEOUT);
+}
+
+
+int main(void){
+    setup();
+    while(1){
+
+    }
+
+}
+
+void setup(){
+    IntMasterEnable();//GENERAL INTERRUPTS
+
+    //Clock 40MHz
+    SysCtlClockSet(SYSCTL_OSC_MAIN| SYSCTL_XTAL_16MHZ | SYSCTL_USE_PLL | SYSCTL_SYSDIV_5);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    //LEDs
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
+
+    //CONFIG TMR0
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0)){}
+    TimerConfigure(TIMET0_BASE, TIMER_CFG_PERIODIC);
+    TimerLoadSet(TIMER0_BASE, TIMER_A, SysCtlClockGet()/2 - 1);
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    TimerIntRegister(TIMER0_BASE, TIMER_A, Timer0IntHandler);
+    IntEnable(INT_TIMERA);
+    TimerEnable(TIMER0_BASE, TIMER_A);
+
+    //CONFIG UART
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA)){}
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0)){}
+    GPIOPinConfigure(0x00000001); //RX
+    GPIOPinConfigure(0x00000401); //TX
+    GPIOinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    UARTConfigSetExpClk(UART0_NASE, SysCtlClockGet(), 115200, UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE);
+    IntEnable(INT_UART0);
+    UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+    UARTIntClear(UART0_BASE, UART_INT_RX | UART_INT_RT);
+    UARTIntRegister(UART0_BASE, UARTIntHandler);
+    UARTEnable(UART_BASE);
+}
+
+
+void UARTIntHandler(){
+    UARTIntClear(UART0_BASE, UART_INT_RX | UART_INT_RT);
+
+}
